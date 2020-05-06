e2577c90337bc4914451ee347b2badd4f3d36b27
lab <lab@ubuntu-cg.jnpr.net>
2020-05-06 17:26:48 +0200

    solved connect_ns.sh addressing issue

--- scripts/connect_ns.sh
+++ scripts/connect_ns.sh
@@ -10,6 +10,14 @@ print_help () {
 	echo "useage: $0 -s <src_ns> -d <dst_ns> -p <prefix30> [OPTIONAL] -h (print help)"
 }
 
+nextIp () {
+    IP=$1
+    IP_HEX=$(printf '%.2X%.2X%.2X%.2X\n' `echo $IP | sed -e 's/\./ /g'`)
+    NEXT_IP_HEX=$(printf %.8X `echo $(( 0x$IP_HEX + 1 ))`)
+    NEXT_IP=$(printf '%d.%d.%d.%d\n' `echo $NEXT_IP_HEX | sed -r 's/(..)/0x\1 /g'`)
+    echo "$NEXT_IP"
+}
+
 
 
 PARAMS=""
@@ -53,7 +61,6 @@ while (( "$#" )); do
 done
 # set positional arguments in their propper place
 eval set -- "$PARAMS"
-
 if [[ -n $SRC ]] && [[ -n $DST ]]; then
     #create veth pair with name src-dst
     sudo ip link add ${s_if}-${d_if} type veth peer name ${d_if}-${s_if}
@@ -62,9 +69,12 @@ if [[ -n $SRC ]] && [[ -n $DST ]]; then
     sudo ip -n $SRC link set ${s_if}-${d_if} up
     sudo ip -n $DST link set ${d_if}-${s_if} up
     if [[ -n $PREFIX_30 ]]; then
-        #[TO-DO] fix this hardcoding to proper implement this functionality
-        sudo ip -n $SRC addr add $PREFIX_30.1 dev ${s_if}-${d_if}
-        sudo ip -n $DST addr add $PREFIX_30.2 dev ${d_if}-${s_if}
+        IP_1=$(nextIp $PREFIX_30)
+	echo "${s_if}-${d_if} interface has: ${IP_1}"
+	IP_2=$(nextIp $IP_1)
+	echo "${d_if}-${s_if} interface has: $IP_2"
+        sudo ip -n $SRC addr add $IP_1 dev ${s_if}-${d_if}
+        sudo ip -n $DST addr add $IP_2 dev ${d_if}-${s_if}
     fi
 else
     print_help
